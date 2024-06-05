4f2de7d9afff88a67e5ac5c733d7a3cfa6642d0f
GalangNour <47129139+GalangNour@users.noreply.github.com>
2024-06-05 22:07:37 +0700

    Update ApiKey.kt

--- data/src/main/java/com/example/data/network/ApiKey.kt
+++ data/src/main/java/com/example/data/network/ApiKey.kt
@@ -1,7 +1,6 @@
 package com.example.data.network
 
 object ApiKey {
-    const val apiKey = "5d75046fdec66c525feefb593c2604d6"
-    const val apiBearer = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDc1MDQ2ZmRlYzY2YzUyNWZlZWZiNTkzY" +
-            "zI2MDRkNiIsInN1YiI6IjY2M2ExZGMxOTU5MGUzMDEyM2JlMTVkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._h1EldmpBhBd4tr81r7mcNly0tw6CNCe4tzQA6fXl2c"
+    const val apiKey = ""
+    const val apiBearer = ""
 }
